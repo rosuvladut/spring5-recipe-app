658e5eb7ca81e26b5223e47bb2ec0d76d78f91de
rosuv <rosuvladut@gmail.com>
2018-05-10 17:35:35 +0300

    add recipe

---
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -0,0 +1,10 @@
+package guru.springframework.domain;
+
+public class Recipe {
+    private String description;
+    private Integer preptTime;
+    private Integer cookTime;
+    private Integer servings;
+    private String source;
+}
+
